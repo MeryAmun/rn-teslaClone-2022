f02e16ba5f5fef6b6c1a05226ce2879694e606c7
MeryAmun <chebesiawah@gmail.com>
2022-04-05 03:01:41 +0100

    render text

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+      <View style={styles.titles}>
+        <Text style={styles.title}> Model 5</Text>
+        <Text style={styles.subtitles}>Starting at $69,420</Text>
+      </View>
+      </View>
+      
     </View>
   );
 }
@@ -17,4 +22,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer:{
+width:'100%',
+height:'100%'
+  },
+  titles:{
+    marginTop:'30%',
+    width:'100%',
+    alignItems:'center'
+  },
+  title:{
+    fontSize:40,
+    fontWeight:'500'
+  },
+  subtitles:{
+fontSize:16,
+color:'#5c5e62'
+  }
 });
+
+
+
